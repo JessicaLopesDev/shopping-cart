7895a329ad71d5b9649a7d4e7fddbff3bd3fe68f
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-01-16 18:30:06 -0300

    Requisito 4 ok

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
-const productsList = await fetchProductsList('computador');
-
 const productsContainer = document.querySelector('.products');
 
-productsList.map((item) => productsContainer.appendChild(createProductElement(item)));
+const productsList = await fetchProductsList('computador');
+if (productsList) {
+  productsContainer.innerHTML = '';
+  productsList.map((item) => productsContainer.appendChild(createProductElement(item)));
+}
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
