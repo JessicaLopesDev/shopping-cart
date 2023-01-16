06900c6724d38a14d74e2681a0ff1f00b82fd7fd
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-01-16 17:03:27 -0300

    Requisito 2 ok

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,12 @@
 export const fetchProduct = () => {
-  // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (product) => {
+  if (!product) throw new Error('Termo de busca não informado');
+
+  const fetchApi = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  const data = await fetchApi.json();
+  const result = await data.results;
+
+  return result;
 };
